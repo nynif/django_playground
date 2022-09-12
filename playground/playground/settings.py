36ecc134757942bae9f51ffa1848cf00204b9c42
nynif <nynif@proton.me>
2022-09-12 10:58:58 +0200

    add sharded header and footer template

--- playground/playground/settings.py
+++ playground/playground/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'playground.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -69,6 +69,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'playground.wsgi.application'
 
