0a879ceb65491e4ed696b04b7e872cf09ce589fa
nynif <nynif@proton.me>
2022-09-13 10:06:22 +0200

    test some filter on homepage

--- src/page_home/views.py
+++ src/page_home/views.py
@@ -2,7 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
+    _list = [x for x in range(10)]
+    #_list = []
     context = {
         'name': "ANTOINE",
+        'mylist': _list,
 	}
     return render(request, 'page_home/index.html', context)
